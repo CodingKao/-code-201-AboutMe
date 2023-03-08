f11cd0275f897d34793360a1cdf88219d23fcaa9
Kao Saelor <saelor.kao@gmail.com>
2023-03-07 21:27:09 -0800

    css/js

--- js/script.js
+++ js/script.js
@@ -3,21 +3,21 @@
 'use strict';
 
 
-//Initial greetings prompt
-// eslint-disable-next-line no-unused-vars
-function greetings(){
-  alert('Hello! Welcome to my About me webpage.');
-  let userName = prompt('First what is your name?');
-  console.log('userName:' + userName);
-  alert('Hello, ' + userName + '.  Let\'s see how well you know me.');
+// //Initial greetings prompt
+// // eslint-disable-next-line no-unused-vars
+// function greetings(){
+//   alert('Hello! Welcome to my About me webpage.');
+//   let userName = prompt('First what is your name?').toLowerCase();
+//   console.log('userName:' + userName);
+//   alert('Hello, ' + userName + '.  Let\'s see how well you know me.');
 
-  return userName;
-}
+//   return userName;
+// }
 
 
 // 1st question
 // prompt 1st question
-let thailand = prompt('Was I born in Thailand? y/n or yes/no');
+let thailand = prompt('Was I born in Thailand? y/n or yes/no').toLowerCase();
 console.log('Born in Thailand: ' + thailand);
 
 //if user guess is correct
@@ -29,7 +29,7 @@ if (thailand === 'yes' || thailand === 'y') {
 
 // 2nd question
 // prompt 2nd question
-let marines = prompt('Did I served in the Marine Corps? y/n or yes/no');
+let marines = prompt('Did I served in the Marine Corps? y/n or yes/no').toLowerCase();
 console.log('In the Marnes: ' + marines);
 
 //if user guess is correct
@@ -41,7 +41,7 @@ if (marines === 'yes' || marines === 'y') {
 
 // 3rd question
 // prompt 3rd question
-let psu = prompt('Did I go to Portland State University? y/n or yes/no');
+let psu = prompt('Did I go to Portland State University? y/n or yes/no').toLowerCase();
 console.log('Go to PSU: ' + psu);
 
 //if user guess is correct
@@ -53,7 +53,7 @@ if (psu === 'yes' || psu === 'y') {
 
 // 4th question
 // prompt 4th question
-let up = prompt('Did I go to University of Portland? y/n or yes/no');
+let up = prompt('Did I go to University of Portland? y/n or yes/no').toLowerCase();
 console.log('Go to UP: ' + up);
 
 //if user guess is correct
@@ -65,7 +65,7 @@ if (up === 'yes' || up === 'y') {
 
 // 5th question
 // prompt 5th question
-let codefellow = prompt('Am I currently attending Code Fellows? y/n or yes/no');
+let codefellow = prompt('Am I currently attending Code Fellows? y/n or yes/no').toLowerCase();
 console.log('Attending Code Fellows: ' + codefellow);
 
 //if user guess is correct
@@ -76,4 +76,4 @@ if (codefellow === 'yes' || codefellow === 'y') {
 }
 
 // Goodbye message
-alert("Thank you for vising, " + userName + ".  See you next time.")
+alert('Thank you for vising, ' + userName + '.  See you next time.');
