14f1a282bb5b7d426e44c48eab56e2a10fed38ce
Kao Saelor <saelor.kao@gmail.com>
2023-03-10 02:12:17 -0800

    redo code

--- js/script.js
+++ js/script.js
@@ -11,7 +11,7 @@ let userName;
 function greetings() {
   alert('Hello! Welcome to my About me webpage.');
   userName = prompt('First what is your name?');
-  console.log("userName:" + userName);
+  console.log('userName:' + userName);
   alert('Hello, ' + userName + '.  Let\'s see how well you know me.');
 }
 greetings();
@@ -22,7 +22,7 @@ function questionOne() {
 
   let thailand = prompt('Was I born in Thailand? y/n or yes/no').toLowerCase();
   console.log('Born in Thailand: ' + thailand);
-  
+
   // if user guess is correct
   if (thailand === 'yes' || thailand === 'y') {
     alert('Yes, that\'s correct!');
@@ -39,7 +39,7 @@ function questionTwo() {
 
   let marines = prompt('Did I served in the Marine Corps? y/n or yes/no').toLowerCase();
   console.log('In the Marnes: ' + marines);
-  
+
   // if user guess is correct
   if (marines === 'yes' || marines === 'y') {
     alert('Yes, that\'s correct! I did. Semper Fi.');
@@ -56,7 +56,7 @@ function questionThree() {
 
   let psu = prompt('Did I go to Portland State University? y/n or yes/no').toLowerCase();
   console.log('Go to PSU: ' + psu);
-  
+
   // if user guess is correct
   if (psu === 'yes' || psu === 'y') {
     alert('Yes, that\'s correct! I did. Go Vikings!');
@@ -74,7 +74,7 @@ function questionFour() {
 
   let up = prompt('Did I go to University of Portland? y/n or yes/no').toLowerCase();
   console.log('Go to UP: ' + up);
-  
+
   //if user guess is correct
   if (up === 'yes' || up === 'y') {
     alert('Yes, that\'s correct!');
@@ -91,7 +91,7 @@ function questionFive() {
 
   let codefellow = prompt('Am I currently attending Code Fellows? y/n or yes/no').toLowerCase();
   console.log('Attending Code Fellows: ' + codefellow);
-  
+
   // if user guess is correct
   if (codefellow === 'yes' || codefellow === 'y') {
     alert('Yes, I am!');
@@ -102,110 +102,86 @@ function questionFive() {
 }
 questionFive();
 
-// 6th question for loop
-// Secert Number
+
+alert('Let\'s play a guessing game.');
+alert('I\'m thinking of a number between 1 - 10.  Can you guess the secert number? I\'ll give you 4 attempts');
+
+
+// Questions 6
 function questionSix() {
 
-  let secretNum = 7;
-  
-  // Counter to track # of guesses
+  let secertNum = '7';
   let counter = 0;
-  
-  alert('Let\'s play a guessing game.');
-  
-  // loop until counter is 4
-  while(counter < 4) {
-    
-    // prompt user for guess
-    
-    let userGuess= prompt('I\'m thinking of a number between 1 - 10.  Can you guess it? You have 4 attempts?');
-    console.log('You guessed: ' + userGuess);
+
+  while (counter < 4) {
+    let guessNum = prompt('Pick a number from 0 - 10');
     counter++;
-    
-    // validate the guess is equal to the secret num
-    if (Number(userGuess) === secretNum) {
-      // if user guess corectly alert and end loop
-      alert('Yes, you guessed correctly.  Good Job!');
+
+    if (guessNum === secertNum) {
+      alert('Great job! That is correct!');
       score++;
       break;
-      
-      // if the guess is too high, prompt alert
-    } else if (Number(userGuess) > secretNum) {
-      alert('Your guess is too high!');
-      console.log('You guessed: ' + userGuess);
-      
-      // if user guess is too low, prompt alert
-    } else if (Number(userGuess) < secretNum) {
-      alert('Your guess is too low!');
-      console.log('You guessed: ' + userGuess);
-      
+
+    } else if (guessNum < secertNum) {
+      alert('Too low, please try again');
+
+    } else if (guessNum > secertNum) {
+      alert('Too high, please try again');
+
     }
-    
-    
+
     if (counter === 0) {
-      alert('You have 4 more attempts!!');
+      alert('You have 4 more attempts!');
     } else if (counter === 1) {
-      alert('You have 3 more attempts!!');
+      alert('You have 3 more attempts!');
     } else if (counter === 2) {
-      alert('You have 2 more attempts!!');
+      alert('You have 2 more attempts!');
     } else if (counter === 3) {
       alert('This is your last attempt!');
     }
-    
+
   }
+
+  alert('The secret number is ' + secertNum);
 }
+
 questionSix();
-// end after 4 gusses
-alert('The secert number is ' + secretNum);
 
 
-// 7th question for loop with arrays
+// Question 7
+function questionSeven() {
+  let color = [
+    'red',
+    'green',
+    'blue',
+  ];
 
-// Define answer
-let answers = ['red', 'green', 'blue'];
+  let attempt = 6;
 
-// how many attempts for user
-let attempts = 0;
+  let correctGuess = false;
 
-while (attempts < 6) {
-  let userGuess2 = prompt('What are the primary colors used in programming languages? Hint: RBG');
-  console.log(userGuess2);
-  attempts++;
-
-  if (userGuess2 === answers[0]) {
-    alert('Yes! Red is one of the colors.  Good job!');
+  while (attempt > 0) {
+    let userGuess2 = prompt('What are the primary colors used in programming languages? Hint: RBG').toLowerCase();
     console.log(userGuess2);
-    score++;
-    break;
-
-  } else if (userGuess2 === answers[1]) {
-    alert('Yes! Green is one of the colors.  Good Job!');
-    console.log(userGuess2);
-    score++;
-    break;
-
-  } else if (userGuess2 === answers[2]) {
-    alert('Yes! Blue is one of the colors.  Good Job!');
-    console.log(userGuess2);
-    score++;
-    break;
-
-  } else if (userGuess2 === '') {
-    alert('You have to pick a color!');
-    console.log(userGuess2);
-
-  } else if (userGuess2 !== '') {
-    alert('Wrong. Try again.');
-    console.log(userGuess2);
-
+    for (let i = 0; i < color.length; i++) {
+      if (userGuess2 === color[i]) {
+        alert('Good job! You answered correctly!');
+        score++;
+        correctGuess = true;
+        break;
+      }
+    }
+    attempt--;
+    if (correctGuess === true) {
+      break;
+    }
   }
-
+  if (attempt === 0) {
+    alert('The primary colors used in programming languages are Red, Green, and Blue (RGB).');
+  }
 }
+questionSeven();
 
-alert('The primary colors used in programming languages are Red, Green, and Blue (RGB).');
-
-while (score <= 7) {
-  score = alert('You got ' + score + ' questions correct!');
-}
+score = alert('You got ' + score + ' questions correct!');
 
 alert('Thank you for visiting.  I hope you come back again ' + userName + '!');
